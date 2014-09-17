6b5d4c2d763e19b1feee85f1a6894eb760ffc192
Brian So <bcso@uwaterloo.ca>
2014-09-17 15:14:11 -0400

    added header' '

---
+++ header.cpp
@@ -0,0 +1,23 @@
+//*********************************************
+// Student1 Name: Jonathan Wen
+// Student1 Number: 20515143
+//
+// Student2 Name: Brian So
+// Student2 Number: 20477254
+//
+// SYDE 121 Lab: 01 Assignment: 
+//
+// Filename: lab0101.cpp
+// Date submitted: XXX
+//
+// We hereby declare that this code, submitted
+// for credit for the course SYDE121, is a product
+// of our own efforts. This coded solution has
+// not been plagiarized from other sources and
+// as not been knowingly plagiarized by others.
+//
+// Pair Programming Work Declaration:
+// Student1 Name Completed ~XXX% of the assignment.
+// Student2 Name Completed ~XXX% of the assignment.
+//
+//*********************************************
