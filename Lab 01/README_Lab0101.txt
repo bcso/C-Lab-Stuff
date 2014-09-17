80d72e779145a973ab4bb87adf23e942da364fde
Brian So <bcso@uwaterloo.ca>
2014-09-17 15:48:17 -0400

    Added bonus question, finished both READMEs

---
+++ Lab 01/README_Lab0101.txt
@@ -0,0 +1,27 @@
+//********************************************* 
+// Student1 Name: Jonathan Wen 
+// Student1 Number: 20515413
+// 
+// Student2 Name: Brian So
+// Student2 Number: 20477254
+// 
+// SYDE 121 Lab: 1 Assignment: 1
+// 
+// Filename: README_Lab0101.txt
+// Date submitted: Sep 17, 2014
+// 
+// We hereby declare that this code, submitted 
+// for credit for the course SYDE121, is a product 
+// of our own efforts. This coded solution has 
+// not been plagiarized from other sources and 
+// as not been knowingly plagiarized by others. 
+// 
+// Pair Programming Work Declaration: 
+// Student1 Jonathan Wen Completed ~50% of the assignment. 
+// Student2 Brian So Completed ~50% of the assignment. 
+// 
+//********************************************* 
+
+Instructions to run the program: 
+Step 1. Compile and run Lab0101.cpp. 
+Step 2. Press any key to dismiss the window. 
---
+++ Lab 01/lab0101.cpp
@@ -0,0 +1,48 @@
+//********************************************* 
+// Student1 Name: Jonathan Wen 
+// Student1 Number: 20515413
+// 
+// Student2 Name: Brian So
+// Student2 Number: 20477254
+// 
+// SYDE 121 Lab: 1 Assignment: 1
+// 
+// Filename: Lab0101.cpp
+// Date submitted: Sep 17, 2014
+// 
+// We hereby declare that this code, submitted 
+// for credit for the course SYDE121, is a product 
+// of our own efforts. This coded solution has 
+// not been plagiarized from other sources and 
+// as not been knowingly plagiarized by others. 
+// 
+// Pair Programming Work Declaration: 
+// Student1 Jonathan Wen Completed ~50% of the assignment. 
+// Student2 Brian So Completed ~50% of the assignment. 
+// 
+//********************************************* 
+
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	
+	int i=0;
+	
+	for (int i=3; i<10; i++)
+	{
+		cout << "i = " << i 
+ 		<< " squared = " 
+ 		<< i * i 
+		<< endl; 
+	}
+	cout << "\nA program by: Jonathan Wen and Brian So!\n"; 
+ 
+ 	system("pause"); 
+ 
+ 	return 0; 
+
+}
