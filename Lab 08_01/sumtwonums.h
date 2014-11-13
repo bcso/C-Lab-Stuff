5401ce59223493204c85e9d3b71665b5d52cea72
bcso <bcso@uwaterloo.ca>
2014-11-13 16:57:21 -0500

    added header to header..dawg

--- Lab 08_01/sumtwonums.h
+++ Lab 08_01/sumtwonums.h
@@ -1,8 +1,34 @@
+//********************************************* 
+// Student1 Name: Brian So  
+// Student1 Number: 20477254
+// 
+// Student2 Name: Wayne Wu
+// Student2 Number: 20563585
+// 
+// SYDE 121 Lab: 08 Assignment: 01
+// 
+// Filename: sumtwonums.h
+// Date submitted: 13/11/14
+// 
+// We hereby declare that this code, submitted 
+// for credit for the course SYDE121, is a product 
+// of our own efforts. This coded solution has 
+// not been plagiarized from other sources and 
+// as not been knowingly plagiarized by others. 
+// 
+// Pair Programming Work Declaration: 
+// Wayne Wu Completed 50% of the assignment. 
+// Brian So Completed 50% of the assignment. 
+// 
+//*********************************************
+
+
 //Display input
 void displayInput(int input[], int size);
 
 //Do the computation
 void compute(int firstNumber[], int secondNumber[], int count1, int count2);
 
+//Add the augend, addend and carry
 int add(int a, int b, int carry);
 
